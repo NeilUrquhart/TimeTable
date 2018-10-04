68fb15baf262ecdd113c481c0726c72727141ef4
ExtremelySalty <neil.earlam123@icloud.com>
2018-10-04 21:52:47 +0100

    Initial Commit

---
+++ TimeTable/src/main/java/TimeTable/TimeTable/App.java
@@ -0,0 +1,13 @@
+package TimeTable.TimeTable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
